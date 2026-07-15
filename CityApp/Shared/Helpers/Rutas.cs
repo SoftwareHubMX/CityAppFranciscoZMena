3b3004caef69032a70fd2dafbc5df438fae5703d
SoftwareHubMX <oliver.softhubmx@gmail.com>
2026-07-15 13:37:27 -0600

    ActualizacionColoresCadenaConexionRutas

--- CityApp/Shared/Helpers/Rutas.cs
+++ CityApp/Shared/Helpers/Rutas.cs
@@ -9,31 +9,31 @@ namespace CityApp.Shared.Helpers
     public class Rutas
     {
         public static string RutaEvidenciaSolucionReporteCiudadano
-        { get; } = "C:\\apps\\CityAppPachucaArchivos\\RutaEvidenciaSolucionReporteCiudadano\\";
+        { get; } = "C:\\apps\\CityAppPachucaArchivos\\CityAppFranciscoZMena\\RutaEvidenciaSolucionReporteCiudadano\\";
 
         public static string RutaEvidenciaReporteCiudadano
-        { get; } = "C:\\apps\\CityAppPachucaArchivos\\ReportesCiudadanos\\";
+        { get; } = "C:\\apps\\CityAppPachucaArchivos\\CityAppFranciscoZMena\\ReportesCiudadanos\\";
 
         public static string RutaMultimediaNoticias
-        { get; } = "C:\\apps\\CityAppPachucaArchivos\\Noticias\\";
+        { get; } = "C:\\apps\\CityAppPachucaArchivos\\CityAppFranciscoZMena\\Noticias\\";
 
         public static string RutaMultimediaAgendas
-        { get; } = "C:\\apps\\CityAppPachucaArchivos\\Agendas\\";
+        { get; } = "C:\\apps\\CityAppPachucaArchivos\\CityAppFranciscoZMena\\Agendas\\";
 
         public static string RutaMultimediaTurismo
-        { get; } = "C:\\apps\\CityAppPachucaArchivos\\Turismo\\";
+        { get; } = "C:\\apps\\CityAppPachucaArchivos\\CityAppFranciscoZMena\\Turismo\\";
 
         public static string RutaMultimediaSliders
-        { get; } = "C:\\apps\\CityAppPachucaArchivos\\Sliders\\";
+        { get; } = "C:\\apps\\CityAppPachucaArchivos\\CityAppFranciscoZMena\\Sliders\\";
 
         public static string RutaMultimediaNormatividades
-        { get; } = "C:\\apps\\CityAppPachucaArchivos\\Normatividades\\";
+        { get; } = "C:\\apps\\CityAppPachucaArchivos\\CityAppFranciscoZMena\\Normatividades\\";
 
         public static string RutaMultimediaNormatividadesPublic
         { get; } = "C:\\apps\\CityAppMineralDeLaReforma\\wwwroot\\";
 
         public static string RutaHistoricosPredios
-        { get; } = "C:\\apps\\CityAppPachucaArchivos\\HistoricosPredios\\";
+        { get; } = "C:\\apps\\CityAppPachucaArchivos\\CityAppFranciscoZMena\\HistoricosPredios\\";
 
         public static string RutaTemporalArchivos
         { get; } = "C:\\apps\\CityAppMineralDeLaReforma\\wwwroot\\TemporalFiles\\";
@@ -42,13 +42,13 @@ namespace CityApp.Shared.Helpers
         { get; } = "C:\\apps\\Archivos\\wwwroot\\";
 
         public static string RutaMultimediaDirectorio
-        { get; } = "C:\\apps\\CityAppPachucaArchivos\\Directorios\\";
+        { get; } = "C:\\apps\\CityAppPachucaArchivos\\CityAppFranciscoZMena\\Directorios\\";
 
         public static string RutaMultimediaArchivoAnuncio
-        { get; } = "C:\\apps\\CityAppPachucaArchivos\\Anuncios\\";
+        { get; } = "C:\\apps\\CityAppPachucaArchivos\\CityAppFranciscoZMena\\Anuncios\\";
 
         public static string RutaMultimediaSolicitudes
-        { get; } = "C:\\apps\\CityAppPachucaArchivos\\SolicitudesPoda\\";
+        { get; } = "C:\\apps\\CityAppPachucaArchivos\\CityAppFranciscoZMena\\SolicitudesPoda\\";
 
     }
 }
